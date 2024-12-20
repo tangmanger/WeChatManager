cbc9131b2ce159686fa4de7504ed5b2df743d3dd
tangmanger <tangmanger@outlook.com>
2024-12-21 00:44:04 +0800

    fix  init

---
+++ WeChatManager/Controls/IconRadioButton.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace WeChatManager.Controls
+{
+    public class IconRadioButton : RadioButton
+    {
+
+
+        public string Icon
+        {
+            get { return (string)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(string), typeof(IconRadioButton));
+
+
+        public SolidColorBrush IconForeground
+        {
+            get { return (SolidColorBrush)GetValue(IconForegroundProperty); }
+            set { SetValue(IconForegroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconForeground.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconForegroundProperty =
+            DependencyProperty.Register("IconForeground", typeof(SolidColorBrush), typeof(IconRadioButton));
+
+
+
+        public double IconSize
+        {
+            get { return (double)GetValue(IconSizeProperty); }
+            set { SetValue(IconSizeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconSizeProperty =
+            DependencyProperty.Register("IconSize", typeof(double), typeof(IconRadioButton), new PropertyMetadata(14d));
+
+
+    }
+}
